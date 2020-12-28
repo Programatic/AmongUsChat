8684fa39f047ae1a972ed37ed4683afeca27f538
Ford Smith <fordmsmith13@gmail.com>
2020-12-28 16:08:13 -0600

    Working audio streaming

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use std::process::Command;
+#![allow(non_snake_case)]
 
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Data, DefaultStreamConfigError,
+};
+use magnum_opus::{Decoder, Encoder};
+use rubato::Resampler;
 use serde::Deserialize;
+use std::fs::File;
+use std::io::BufWriter;
+use std::process::Command;
+use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Deserialize)]
 struct PlayerState {
@@ -42,43 +52,9 @@ struct SendData<'a> {
     audioData: bool,
 }
 
-use cpal::{
-    traits::{DeviceTrait, HostTrait, StreamTrait},
-    DefaultStreamConfigError,
-};
-use std::fs::File;
-use std::io::BufWriter;
-use std::sync::{Arc, Mutex};
-
-#[derive(Debug)]
-struct Opt {
-    #[cfg(all(
-        any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"),
-        feature = "jack"
-    ))]
-    jack: bool,
-
-    device: String,
-}
-
-impl Opt {
-    fn from_args() -> Self {
-        let app = clap::App::new("beep").arg_from_usage("[DEVICE] 'The audio device to use'");
-
-        let matches = app.get_matches();
-        let device = matches.value_of("DEVICE").unwrap_or("default").to_string();
-
-        #[cfg(any(
-            not(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd")),
-            not(feature = "jack")
-        ))]
-        Opt { device }
-    }
-}
-
 fn main() -> Result<(), anyhow::Error> {
     // let foo = Command::new("test.exe").output().unwrap();
-
+    //
     // const PIPE: &str = "\\\\.\\pipe\\amonguspipe";
     // let mut f: std::fs::File;
     // loop {
@@ -90,15 +66,15 @@ fn main() -> Result<(), anyhow::Error> {
     //         println!("{}", x);
     //     }
     // }
-
+    //
     // let mut currentPlayerState: Arc<Mutex<PlayerState>> =
     //     Arc::new(Mutex::new(PlayerState::default()));
     // let mut currentPlayerState2 = currentPlayerState.clone();
     // let mut currentPlayerState3 = currentPlayerState.clone();
-
+    //
     // let h1 = std::thread::spawn(move || {
     //     use std::io::Read;
-
+    //
     //     let mut r: [u8; 1024] = [0; 1024];
     //     while true {
     //         let len = f.read(&mut r).unwrap();
@@ -108,7 +84,7 @@ fn main() -> Result<(), anyhow::Error> {
     //         *stateLock = currentPlayerStateInbound;
     //     }
     // });
-
+    //
     // let h2 = std::thread::spawn(move || {
     //     while true {
     //         let lock = currentPlayerState3.lock().unwrap();
@@ -118,98 +94,118 @@ fn main() -> Result<(), anyhow::Error> {
     //     }
     // });
 
-    let enc = opus::Encoder::new(48000, opus::Channels::Stereo, opus::Application::Voip);
+    let mut encoder = magnum_opus::Encoder::new(
+        48000,
+        magnum_opus::Channels::Stereo,
+        magnum_opus::Application::Voip,
+    )
+    .unwrap();
+    let mut decoder = magnum_opus::Decoder::new(48000, magnum_opus::Channels::Stereo)?;
 
-    // std::thread::sleep(std::time::Duration::from_secs(10));
+    let mut resampler = rubato::FftFixedInOut::<f32>::new(44100, 48000, 896, 2);
 
-    // return Ok(());
-
-    let opt = Opt::from_args();
-
-    #[cfg(any(
-        not(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd")),
-        not(feature = "jack")
-    ))]
     let host = cpal::default_host();
 
-    // Setup the input device and stream with the default input config.
-    let device = if opt.device == "default" {
-        host.default_input_device()
-    } else {
-        host.input_devices()?
-            .find(|x| x.name().map(|y| y == opt.device).unwrap_or(false))
-    }
-    .expect("failed to find input device");
+    let device = host.default_input_device().unwrap();
 
     println!("Input device: {}", device.name()?);
 
     let config = device
         .default_input_config()
         .expect("Failed to get default input config");
+
     println!("Default input config: {:?}", config);
 
-    // The WAV file we're recording to.
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
-
-    // let spec = wav_spec_from_config(&config);
-    // let writer = hound::WavWriter::create(PATH, spec)?;
-    let encoder = Arc::new(Mutex::new(Some(writer)));
-
-    // A flag to indicate that recording is in progress.
     println!("Begin recording...");
-
-    // Run the input stream on a separate thread.
-    let encoder_2 = encoder.clone();
 
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
 
-    let stream = match config.sample_format() {
-        cpal::SampleFormat::F32 => device.build_input_stream(
+    let sample_format = config.sample_format();
+
+    let mut socket = std::net::UdpSocket::bind("192.168.1.82:1337")?;
+    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.raw");
+    // let socket = std::fs::File::create(PATH).unwrap();
+    // let mut socket = BufWriter::new(socket);
+
+    let stream = device
+        .build_input_stream_raw(
             &config.into(),
-            move |data, _: &_| write_input_data::<f32>(data, &writer_2),
+            sample_format,
+            move |data, _: &_| write_input_data_f32(data, &mut encoder, &mut decoder, &mut socket),
             err_fn,
-        )?,
-        cpal::SampleFormat::I16 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i16>(data, &writer_2),
-            err_fn,
-        )?,
-        cpal::SampleFormat::U16 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<u16>(data, &writer_2),
-            err_fn,
-        )?,
-    };
+        )
+        .unwrap();
 
     stream.play()?;
 
-    // Let recording go for roughly three seconds.
-    std::thread::sleep(std::time::Duration::from_secs(10));
+    std::thread::sleep(std::time::Duration::from_secs(7200));
     drop(stream);
-    // writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", PATH);
 
     Ok(())
 }
 
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
+type ResamplerHandle = rubato::FftFixedInOut<f32>;
 
-fn write_input_data<T>(input: &[T], writer: &WavWriterHandle)
-where
-    T: cpal::Sample,
-{
-    println!("{}", input);
-    // if let Ok(mut guard) = writer.try_lock() {
-    //     if let Some(writer) = guard.as_mut() {
-    //         for &sample in input.iter() {
-    //             let sample: U = cpal::Sample::from(&sample);
-    //             writer.write_sample(sample).ok();
-    //         }
-    //     }
+fn write_input_data_f32(
+    input: &Data,
+    encoder: &mut Encoder,
+    decoder: &mut Decoder,
+    socket: &mut std::net::UdpSocket,
+) {
+    let mut inp = input.as_slice::<f32>().unwrap().to_vec();
+
+    // inp.append(&mut vec![0f32; 960 - inp.len()]);
+
+    // inp.truncate(resampler.nbr_frames_needed());
+    // if inp.len() < resampler.nbr_frames_needed() {
+    //     inp.append(&mut vec![0f32; resampler.nbr_frames_needed() - inp.len()]);
     // }
+    // let mut wave_out = resampler.process(&vec![Vec::from(inp); 2]).unwrap();//[0].to_owned();
+
+    // use itertools::interleave;
+    // let v1 = wave_out[0].to_owned();
+    // let v2 = wave_out[1].to_owned();
+    // let v = interleave(v1.chunks(1), v2.chunks(1)).flatten().copied().collect::<Vec<f32>>();
+
+    // let buff = encoder.encode_vec_float(v.as_slice(), 960).unwrap();
+    // let buff = encoder.encode_vec_float(&inp[..], 960).unwrap();
+
+    // let mut o = Vec::with_capacity(960);
+    // let mut o = [0f32; 960];
+    // decoder.decode_float(&buff[..], &mut o[..], false);
+
+    // println!("{:?} \n\n\n\n {:?}", inp, o);
+
+    // todo!();
+
+    let slice_u8 = encoder.encode_vec_float(&inp, 1500).unwrap();
+
+    // let slice_u8: &[u8] = unsafe {
+    //     std::slice::from_raw_parts(inp.as_ptr() as *const u8, inp.len() * std::mem::size_of::<f32>())
+    // };
+
+    // println!("{:?}", slice_u8.len());
+
+    // todo!();
+
+    let mut b = [0u8; 1];
+    b[0] = slice_u8.len() as u8;
+
+    // let b = unsafe {
+    //     std::slice::from_raw_parts(b.as_ptr() as *const u8, b.len() * 2)
+    // };
+    
+    socket.send_to(&b, "192.168.1.227:1337").expect("Failure to send 1");
+    socket.send_to(&slice_u8[..], "192.168.1.227:1337").expect("Failure to send 2");
+    // todo!();
 }
+
+// let socket = socket.lock().unwrap();
+
+
+// todo!("Do this");
 
 // fn main() -> std::io::Result<()> {
 //     // let foo = Command::new("test.exe")
@@ -248,5 +244,102 @@ where
 //         // let s= String::from("Among Us.exe");
 //         // let s = std::ffi::CString::new(s).unwrap();
 //         // println!("\n{}", GetPid(s.into_raw()));
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// fn main() -> anyhow::Result<()> {
+//     let host = cpal::default_host();
+
+//     let device = host.default_output_device().unwrap();
+
+//     println!("Output device: {}", device.name()?);
+
+//     let config = device.default_output_config().unwrap();
+//     println!("Default output config: {:?}", config);
+
+//     match config.sample_format() {
+//         cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
+//         cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
+//         cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
+//     }
+// }
+
+// pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+// where
+//     T: cpal::Sample,
+// {
+//     const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.raw");
+//     let mut f = std::fs::File::open(PATH).unwrap();
+
+//     use std::io::Read;
+//     let mut buff: Vec<u8> = Vec::new();
+//     f.read_to_end(&mut buff);
+
+//     // let mut buff = unsafe {
+//     //     std::slice::from_raw_parts(
+//     //         buff.as_ptr() as *const f32,
+//     //         buff.len() / std::mem::size_of::<f32>(),
+//     //     )
+//     // }
+//     // .to_vec();
+
+//     println!("{:?}", buff);
+
+//     todo!();
+
+//     let mut decoder = magnum_opus::Decoder::new(48000, magnum_opus::Channels::Stereo)?;
+//     let mut output: Vec<f32> = Vec::with_capacity(buff.len());
+
+//     for chunk in buff.chunks(20) {
+//         println!("Succ");
+//         let mut decode_buff = [0f32; 960*2*4];
+//         let out = decoder.decode_float(chunk, &mut decode_buff[..], false)?;
+//         output.extend_from_slice(&decode_buff[..out]);
+//     }
+//     // output.truncate(out);
+
+//     todo!("{}", output.len());
+
+//     let mut dat_index = 0;
+
+//     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+//     let stream = device.build_output_stream(
+//         config,
+//         move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+//             write_data(data, 2, &mut dat_index, &mut output)
+//         },
+//         err_fn,
+//     )?;
+//     stream.play()?;
+
+//     std::thread::sleep(std::time::Duration::from_millis(10000));
+//     // drop(output);
+
+//     Ok(())
+// }
+
+// fn write_data<'a, T>(output: &mut [T], channels: usize, data_index: &mut usize, data: &mut Vec<f32>)
+// where
+//     T: cpal::Sample,
+// {
+//     for frame in output.chunks_mut(channels) {
+//         let value = data.get(*data_index);
+//         if let Some(value) = value {
+//             let value: T = cpal::Sample::from::<f32>(value);
+//             for sample in frame.iter_mut() {
+//                 *sample = value;
+//                 *data_index = *data_index + 1usize;
+//             }
+//         }
 //     }
 // }
