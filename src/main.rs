26a19390fc868b83f4dc5e62c53348ccd303862f
Ford Smith <fordmsmith13@gmail.com>
2020-12-28 23:41:39 -0600

    Working Version

--- src/main.rs
+++ src/main.rs
@@ -100,10 +100,6 @@ fn main() -> Result<(), anyhow::Error> {
         magnum_opus::Application::Voip,
     )
     .unwrap();
-    let mut decoder = magnum_opus::Decoder::new(48000, magnum_opus::Channels::Stereo)?;
-
-    let mut resampler = rubato::FftFixedInOut::<f32>::new(44100, 48000, 896, 2);
-
     let host = cpal::default_host();
 
     let device = host.default_input_device().unwrap();
@@ -113,6 +109,10 @@ fn main() -> Result<(), anyhow::Error> {
     let config = device
         .default_input_config()
         .expect("Failed to get default input config");
+
+    let mut resampler = rubato::FftFixedInOut::<f32>::new(44100, 48000, 896, 2);
+
+    let mut resampler = rubato::FftFixedOut::<f32>::new(44100, 48000, 960, 2, 1);
 
     println!("Default input config: {:?}", config);
 
@@ -124,8 +124,10 @@ fn main() -> Result<(), anyhow::Error> {
 
     let sample_format = config.sample_format();
 
-    let mut socket = std::net::UdpSocket::bind("192.168.1.82:1337")?;
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.raw");
+    // let mut socket = std::net::UdpSocket::bind("192.168.1.82:1337")?;
+    let mut socket = std::net::UdpSocket::bind("127.0.0.1:1337")?;
+
+    // const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.raw");
     // let socket = std::fs::File::create(PATH).unwrap();
     // let mut socket = BufWriter::new(socket);
 
@@ -133,7 +135,9 @@ fn main() -> Result<(), anyhow::Error> {
         .build_input_stream_raw(
             &config.into(),
             sample_format,
-            move |data, _: &_| write_input_data_f32(data, &mut encoder, &mut decoder, &mut socket),
+            move |data, _: &_| {
+                write_input_data_f32(data, &mut encoder, &mut socket, &mut resampler)
+            },
             err_fn,
         )
         .unwrap();
@@ -151,59 +155,31 @@ type ResamplerHandle = rubato::FftFixedInOut<f32>;
 fn write_input_data_f32(
     input: &Data,
     encoder: &mut Encoder,
-    decoder: &mut Decoder,
     socket: &mut std::net::UdpSocket,
+    resampler: &mut rubato::FftFixedOut<f32>,
 ) {
     let mut inp = input.as_slice::<f32>().unwrap().to_vec();
 
-    // inp.append(&mut vec![0f32; 960 - inp.len()]);
-
-    // inp.truncate(resampler.nbr_frames_needed());
-    // if inp.len() < resampler.nbr_frames_needed() {
-    //     inp.append(&mut vec![0f32; resampler.nbr_frames_needed() - inp.len()]);
-    // }
-    // let mut wave_out = resampler.process(&vec![Vec::from(inp); 2]).unwrap();//[0].to_owned();
-
-    // use itertools::interleave;
-    // let v1 = wave_out[0].to_owned();
-    // let v2 = wave_out[1].to_owned();
-    // let v = interleave(v1.chunks(1), v2.chunks(1)).flatten().copied().collect::<Vec<f32>>();
-
-    // let buff = encoder.encode_vec_float(v.as_slice(), 960).unwrap();
-    // let buff = encoder.encode_vec_float(&inp[..], 960).unwrap();
-
-    // let mut o = Vec::with_capacity(960);
-    // let mut o = [0f32; 960];
-    // decoder.decode_float(&buff[..], &mut o[..], false);
-
-    // println!("{:?} \n\n\n\n {:?}", inp, o);
+    println!("{}", inp.len());
 
     // todo!();
+
+    // let inp = resampler.process(&vec![inp; 1]).unwrap()[0].to_owned();
 
     let slice_u8 = encoder.encode_vec_float(&inp, 1500).unwrap();
-
-    // let slice_u8: &[u8] = unsafe {
-    //     std::slice::from_raw_parts(inp.as_ptr() as *const u8, inp.len() * std::mem::size_of::<f32>())
-    // };
-
-    // println!("{:?}", slice_u8.len());
-
-    // todo!();
 
     let mut b = [0u8; 1];
     b[0] = slice_u8.len() as u8;
 
-    // let b = unsafe {
-    //     std::slice::from_raw_parts(b.as_ptr() as *const u8, b.len() * 2)
-    // };
-    
-    socket.send_to(&b, "192.168.1.227:1337").expect("Failure to send 1");
-    socket.send_to(&slice_u8[..], "192.168.1.227:1337").expect("Failure to send 2");
-    // todo!();
+    socket
+        .send_to(&b, "127.0.0.1:1337")
+        .expect("Failure to send 1");
+    socket
+        .send_to(&slice_u8[..], "127.0.0.1:1337")
+        .expect("Failure to send 2");
 }
 
 // let socket = socket.lock().unwrap();
-
 
 // todo!("Do this");
 
@@ -246,15 +222,6 @@ fn write_input_data_f32(
 //         // println!("\n{}", GetPid(s.into_raw()));
 //     }
 // }
-
-
-
-
-
-
-
-
-
 
 // fn main() -> anyhow::Result<()> {
 //     let host = cpal::default_host();
